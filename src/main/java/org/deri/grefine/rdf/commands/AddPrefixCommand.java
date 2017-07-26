b56f57eab42de64b28f3545a8bcf8ab826ee62e5
kburger <burger.github@gmail.com>
2017-07-26 11:21:59 +0200

    Refactored AddPrefixCommand to use a lambda to make the code more concise

--- src/main/java/org/deri/grefine/rdf/commands/AddPrefixCommand.java
+++ src/main/java/org/deri/grefine/rdf/commands/AddPrefixCommand.java
@@ -1,7 +1,6 @@
 package org.deri.grefine.rdf.commands;
 
 import java.io.IOException;
-import java.util.Properties;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -11,12 +10,8 @@ import org.deri.grefine.rdf.app.ApplicationContext;
 import org.deri.grefine.rdf.vocab.PrefixExistException;
 import org.deri.grefine.rdf.vocab.VocabularyImporter;
 import org.json.JSONException;
-import org.json.JSONWriter;
-
-import com.google.refine.Jsonizable;
 
 public class AddPrefixCommand extends RdfCommand {
-
     public AddPrefixCommand(ApplicationContext ctxt) {
         super(ctxt);
     }
@@ -28,29 +23,28 @@ public class AddPrefixCommand extends RdfCommand {
         String uri = request.getParameter("uri").trim();
         String projectId = request.getParameter("project");
         String fetchOption = request.getParameter("fetch");
+        
         try {
             getRdfSchema(request).addPrefix(name, uri);
+            
             if (fetchOption.equals("web")) {
                 String fetchUrl = request.getParameter("fetch-url");
+                
                 if (fetchUrl == null || fetchOption.trim().isEmpty()) {
                     fetchUrl = uri;
                 }
+                
                 getRdfContext().getVocabularySearcher().importAndIndexVocabulary(name, uri,
                         fetchUrl, projectId, new VocabularyImporter());
             }
-            respondJSON(response, new Jsonizable() {
-
-                @Override
-                public void write(JSONWriter writer, Properties options) throws JSONException {
+            
+            respondJSON(response, (writer, options) -> {
                     writer.object();
                     writer.key("code");
                     writer.value("ok");
                     writer.endObject();
-                }
-            });
-        } catch (JSONException e) {
-            respondException(response, e);
-        } catch (PrefixExistException e) {
+                });
+        } catch (JSONException | PrefixExistException e) {
             respondException(response, e);
         } catch (Exception e) {
             response.setCharacterEncoding("UTF-8");
